f559343a567237e4f97fe1cfcf34158a5f6dba77
Mike Chaberski <mchaberski@gmail.com>
2024-04-01 13:41:00 -0400

    utils: add rng state serialize method

--- dlmp1/utils.py
+++ dlmp1/utils.py
@@ -1,16 +1,20 @@
-'''Some helper functions for PyTorch, including:
+"""Some helper functions for PyTorch, including:
     - get_mean_and_std: calculate the mean and std value of dataset.
     - msr_init: net parameter initialization.
     - progress_bar: progress bar mimic xlua.progress.
-'''
+"""
 import os
 import sys
 import time
+import base64
+
+import numpy as np
 
 import torch
 import torch.utils.data
 import torch.nn as nn
 import torch.nn.init as init
+from torch import Tensor
 
 
 def get_mean_and_std(dataset):
@@ -136,3 +140,12 @@ def format_time(seconds):
 # https://discuss.pytorch.org/t/how-do-i-check-the-number-of-parameters-of-a-model/4325
 def count_parameters(model: nn.Module, include_non_trainable: bool = False) -> int:
     return sum(p.numel() for p in model.parameters() if (include_non_trainable or p.requires_grad))
+
+
+def serialize_rng_state_str(state: Tensor) -> str:
+    np_state: np.ndarray = state.clone().detach().cpu().numpy()
+    print(np_state.shape)
+    print(np_state.dtype)
+    np_state_bytes = np_state.tobytes()
+    encoded = base64.b64encode(np_state_bytes).decode('us-ascii')
+    return encoded
