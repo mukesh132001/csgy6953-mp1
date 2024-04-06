d0043fa2617520f865f99ec063e7f6baf273675a
Mike Chaberski <mchaberski@gmail.com>
2024-04-06 18:18:19 -0400

    latextopdf: use default input file

--- report/latextopdf.sh
+++ report/latextopdf.sh
@@ -4,8 +4,9 @@ TEX_FILE=$1
 OUTPUT_DIR=$2
 
 if [[ -z "$TEX_FILE" ]] ; then
-  echo "latextopdf.sh: usage: <TEX_FILE> [OUTPUT_DIR]" >&2
-  exit 1
+#  echo "latextopdf.sh: usage: <TEX_FILE> [OUTPUT_DIR]" >&2
+#  exit 1
+   TEX_FILE=mp1-report.tex
 fi
 
 if [[ -z "$OUTPUT_DIR" ]] ; then
