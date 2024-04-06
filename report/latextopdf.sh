77f203eeb5c933c8b1599f4c72fc8868071995b6
Mike Chaberski <mchaberski@gmail.com>
2024-04-06 17:41:04 -0400

    latextopdf: use pdflatex

--- report/latextopdf.sh
+++ report/latextopdf.sh
@@ -5,10 +5,13 @@ OUTPUT_DIR=$2
 
 if [[ -z "$TEX_FILE" ]] ; then
   echo "latextopdf.sh: usage: <TEX_FILE> [OUTPUT_DIR]" >&2
+  exit 1
 fi
 
 if [[ -z "$OUTPUT_DIR" ]] ; then
   OUTPUT_DIR=$(dirname "$TEX_FILE")/output
 fi
 
-exec latexmk -pdf -f -g -bibtex -deps -synctex=1 -interaction=nonstopmode -output-directory=$OUTPUT_DIR "$TEX_FILE"
+mkdir -p "${OUTPUT_DIR}"
+
+exec pdflatex -bibtex -deps -interaction=nonstopmode -output-directory="${OUTPUT_DIR}" "${TEX_FILE}"
