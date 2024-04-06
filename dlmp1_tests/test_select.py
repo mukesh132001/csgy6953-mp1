1c9af76e9b3236eba3e9b0c2c6b60aab80f963aa
Mike Chaberski <mchaberski@gmail.com>
2024-04-06 17:46:06 -0400

    add test_select

---
+++ dlmp1_tests/test_select.py
@@ -0,0 +1,51 @@
+import os
+import json
+import tempfile
+from unittest import TestCase
+
+import torch.random
+import torch.optim
+import torch.optim.lr_scheduler
+import torchsummary
+
+import dlmp1.train
+from dlmp1.train import Partitioning
+from dlmp1.models.resnet import ResNet18
+from torch.optim.optimizer import Optimizer
+from torch.optim.lr_scheduler import LRScheduler
+from torch.optim.lr_scheduler import MultiStepLR
+from torch.optim.lr_scheduler import ConstantLR
+from torch.optim.lr_scheduler import ExponentialLR
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.optim.lr_scheduler import StepLR
+from torch.optim.lr_scheduler import CosineAnnealingLR
+from dlmp1.train import TrainConfig
+from dlmp1.models.resnet import CustomResNet
+from dlmp1.models.resnet import CustomResNetWithDropout
+from dlmp1.models.resnet import Hyperparametry
+from dlmp1.models.resnet import BlockSpec
+
+import dlmp1.select
+
+class ModuleMethodsTest(TestCase):
+
+    def test_iterate_model_factories(self):
+        factories = dlmp1.select.iterate_model_factories([
+            [2, 1, 1, 1],
+            [3, 3, 3],
+            [3, 4, 3],
+            [2, 5, 3],
+            [2, 4, 3],
+            [2, 5, 2],
+            [2, 4, 2],
+        ])
+        for factory in factories:
+            with self.subTest():
+                model = factory()
+                stats = torchsummary.summary(model, verbose=0)
+                self.assertLessEqual(stats.trainable_params, 5_000_000, msg=model.summary_text)
+                model.eval()
+                with torch.random.fork_rng():
+                    torch.random.manual_seed(498682)
+                    x = torch.randn(1, 3, 32, 32)
+                    model(x)
